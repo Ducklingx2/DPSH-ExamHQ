484b379e57045434f8e85a63cdb9dd0fbeb5f48b
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-07 15:08:09 +0530

    Added Task Templates

--- JS/data.js
+++ JS/data.js
@@ -14,3 +14,42 @@ const subjects = [
     "Sanskrit",
 
 ];
+
+const taskTemplates = [
+
+    {
+        id: 1,
+        name: "Question Paper Submission"
+    },
+
+    {
+        id: 2,
+        name: "Answer Key Submission"
+    },
+
+    {
+        id: 3,
+        name: "Blueprint Submission"
+    },
+
+    {
+        id: 4,
+        name: "Result Verification"
+    },
+
+    {
+        id: 5,
+        name: "Hall Ticket Verification"
+    },
+
+    {
+        id: 6,
+        name: "Invigilation Duty"
+    },
+
+    {
+        id: 7,
+        name: "Marks Submission"
+    }
+
+];
