bbe4bc30574ece488224bc102c3a3544e9320c1f
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-07 19:34:31 +0530

    Wrote taskhandler code

--- Backend/Handlers/taskhandler.go
+++ Backend/Handlers/taskhandler.go
@@ -1,1 +1,22 @@
+package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+
+	tasks := []map[string]interface{}{
+
+		{
+			"id": 1,
+			"title": "Question Paper Submission",
+			"status": "Pending",
+		},
+
+	}
+
+	json.NewEncoder(w).Encode(tasks)
+
+}
