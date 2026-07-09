d16d8e5200baf7dfd8c1bfe9179255c5aefbbce8
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-09 14:40:49 +0530

    Update taskhandler.go

--- Backend/Handlers/taskhandler.go
+++ Backend/Handlers/taskhandler.go
@@ -3,20 +3,44 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"examhq/models"
 )
+
+var Tasks = []models.Task{}
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
 
-	tasks := []map[string]interface{}{
+	w.Header().Set("Content-Type", "application/json")
 
-		{
-			"id": 1,
-			"title": "Question Paper Submission",
-			"status": "Pending",
-		},
+	json.NewEncoder(w).Encode(Tasks)
+
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+	var task models.Task
+
+	err := json.NewDecoder(r.Body).Decode(&task)
+
+	if err != nil {
+
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+
+		return
 
 	}
 
-	json.NewEncoder(w).Encode(tasks)
+	task.ID = len(Tasks) + 1
+
+	if task.Status == "" {
+		task.Status = "Pending"
+	}
+
+	Tasks = append(Tasks, task)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(task)
 
 }
