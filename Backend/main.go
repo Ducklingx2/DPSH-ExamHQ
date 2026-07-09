4c82a0b82a3a3187cedd19419a1446016efc95fd
17thToasterOnTheLeft <drshashiibhushann@gmail.com>
2026-07-09 14:40:24 +0530

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"examhq/handlers"
 )
 
 func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("../frontend")))
 
+	http.HandleFunc("/api/tasks", func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.Method {
+
+		case http.MethodGet:
+			handlers.GetTasks(w, r)
+
+		case http.MethodPost:
+			handlers.CreateTask(w, r)
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+
+		}
+
+	})
+
 	log.Println("Server running on http://localhost:8080")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
